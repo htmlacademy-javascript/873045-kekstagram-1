6fd137dcd4ac861a388a8fc3b19d1ab473bbe9df
Keks <keks@htmlacademy.ru>
2024-08-04 21:21:27 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,6 @@
+import { renderThumbnails } from './render-picture-thumbnails.js';
+import './popup.js';
+import './form-validation.js';
+import { updateZoom } from './image-scale.js';
+window.log (renderThumbnails, updateZoom);
+
